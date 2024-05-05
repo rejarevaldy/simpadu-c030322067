20dc5b73815634995205591d471323d6490f26a0
rejarevaldy <rejarevaldy30@gmail.com>
2024-05-05 17:57:48 +0800

    Fortify login

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.app.dashboard-simpadu', ['type_menu' => 'dashboard']);
+    return view('auth.auth-login');
 });
 
-Route::get('/login', function () {
-    return view('auth.auth-login');
-})->name('login');
-Route::get('/register', function () {
-    return view('auth.auth-register');
-})->name('register');
-Route::get('/forgot', function () {
-    return view('auth.auth-forgot-password');
-})->name('forgot-password');
-Route::get('/reset', function () {
-    return view('auth.auth-reset-password');
-})->name('reset-password');
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.app.dashboard-simpadu', ['type_menu' => 'dashboard']);
+    })->name('home');
+});
 
 
-
-
+// Route::get('/login', function () {
+//     return view('auth.auth-login');
+// })->name('login');
+// Route::get('/register', function () {
+//     return view('auth.auth-register');
+// })->name('register');
+// Route::get('/forgot', function () {
+//     return view('auth.auth-forgot-password');
+// })->name('forgot-password');
+// Route::get('/reset', function () {
+//     return view('auth.auth-reset-password');
+// })->name('reset-password');
